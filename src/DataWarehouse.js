e5cfffd975b63dd30ca4d8e867aaedd32adc1603
Nick Blackwell <nickblackwell82@gmail.com>
2021-09-22 12:32:16 -0700

    move classes to src

---
+++ src/DataWarehouse.js
@@ -0,0 +1,198 @@
+
+const EventEmitter = require("events");
+const chokidar = require('chokidar');
+module.exports=class DataWarehouse extends EventEmitter {
+
+
+
+	constructor(config) {
+
+
+		super();
+
+		var counter = 0;
+
+		this._tree = {
+			category: config.rootCategory,
+			children: {}
+
+		}
+
+		console.log(config.path);
+
+		chokidar.watch(config.path).on('all', (event, path) => {
+
+			if (path.toLowerCase().indexOf('.shp') === path.length - 4) {
+				console.log(path);
+				counter++;
+
+				this._addPath(path.split(config.path).pop());
+				
+			}
+			this._checkIdle();
+
+		});
+
+	}
+
+	getCategories() {
+
+		return new Promise((resolve, reject) => {
+
+			console.log('wait for idle');
+
+			this._onIdle(() => {
+				resolve(this._flattenTreeChildrenBFS(this._tree));
+			});
+
+		})
+
+	}
+
+	_onIdle(fn) {
+
+		if (this._isIdle) {
+			fn();
+			return;
+		}
+
+		this.once('idle', fn);
+
+	}
+
+
+	_addPath(path) {
+
+
+
+		var node = this._tree;
+
+		path.split('/').slice(0, -1).forEach((part) => {
+
+
+			if (typeof node.children[part] == "undefined") {
+				node.children[part] = {
+					category: part,
+					children: {}
+				}
+			}
+
+			node = node.children[part];
+
+		});
+
+
+	}
+
+	_checkIdle() {
+
+		if (this._idleTimer) {
+			clearTimeout(this._idleTimer);
+		}
+
+		this._idleTimer = setTimeout(() => {
+
+			delete this._idleTimer;
+
+			this.emit('idle');
+			this._isIdle=true;
+
+			// console.log(JSON.stringify(this._tree , null, '   '));
+			// console.log(JSON.stringify(this._flattenTreeChildrenBFS(this._tree) , null, '   '))
+			// console.log(this._toSql(this._flattenTreeChildrenBFS(this._tree)));
+
+		}, 1000);
+	}
+
+
+	_flattenTreeChildrenBFS(tree, parent) {
+
+		if ((!parent) || typeof parent == "undefined") {
+			parent = tree.category.toLowerCase();
+		}
+
+		var list = [];
+
+
+
+		//BFS
+
+		var nodes = [{
+			category: '',
+			name: tree.category,
+			children: tree.children
+		}];
+		while (nodes.length > 0) {
+
+			var n = nodes.shift();
+			list.push({
+				type: n.category.toLowerCase(),
+				name: n.name,
+				metadata: {},
+				shortName: n.name
+			});
+
+			Object.keys(n.children).forEach((child) => {
+
+				nodes.push({
+					category: n.name,
+					name: child,
+					children: n.children[child].children
+				});
+
+			});
+
+
+		}
+
+
+		return list.slice(1);
+
+
+	}
+
+	_flattenTreeChildrenDFS(tree, parent) {
+
+		if ((!parent) || typeof parent == "undefined") {
+			parent = tree.category.toLowerCase();
+		}
+
+
+
+		var list = [];
+
+		//DFS
+
+		Object.keys(tree.children).forEach((child) => {
+
+			list.push({
+
+				type: parent,
+				name: child,
+				metadata: {},
+				shortName: child
+
+			});
+
+			list = list.concat(this._flattenTreeCategories(tree.children[child], child));
+
+		});
+
+
+		return list;
+
+	}
+
+
+	_toSql(flatList) {
+
+		return `INSERT INTO $tablename (` + Object.keys(flatList[0]).join(', ') + `) VALUES (` + flatList.map((n) => {
+			n['metadata'] = JSON.stringify(n['metadata']);
+			return '"' + Object.keys(n).map((k) => {
+				return n[k]
+			}).join('", "') + '"';
+		}).join("),\n (") + `);`;
+
+	}
+
+}
